f731fe96767b34027fe062d3a2fb6cbd266279db
Jason McGhee <mcghee.j@berkeley.edu>
2012-03-12 03:39:06 -0700

    improved genRandList

--- quicksort/genRandList.py
+++ quicksort/genRandList.py
@@ -2,9 +2,5 @@ import random
 
 def genRandList(length, minNum, maxNum):
 	mylist = []
-	
-	while length > 0:
-		mylist.append(random.randrange(minNum, maxNum))
-		length -= 1
-
+	for x in range(length): mylist.append(random.randrange(minNum, maxNum+1))
 	return mylist
